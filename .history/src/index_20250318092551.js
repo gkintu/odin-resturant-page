2817d19bd0fc9e4a0eaf368a5118374eb6e3fc53
George Kintu <195527580+gkintu@users.noreply.github.com>
2025-03-18 09:33:14 +0100

    Refactor navigation functionality and improve tab highlighting in index.js

--- .history/src/index_20250318092551.js
+++ .history/src/index_20250318092551.js
@@ -9,6 +9,7 @@ function initializeWebsite() {
   const content = document.getElementById("content");
   const navLinks = document.querySelectorAll("header nav ul li a");
 
+
   function loadHome() {
     content.innerHTML = ""; //clear existing content
     content.appendChild(createHome()); // add new content
@@ -16,7 +17,7 @@ function initializeWebsite() {
 
   function loadAbout() {
     content.innerHTML = "";
-    content.appendChild(createAbout());
+    content.appendChild(createAbout();)
   }
 
   function loadMenu() {
@@ -29,8 +30,10 @@ function initializeWebsite() {
     content.appendChild(createContact());
   }
 
-  //highlight clicked tab
-  function setActiveTab(activeLink) {
+
+
+//highlight clicked tab
+function setActiveTab(activeLink) {
     navLinks.forEach((link) => {
       if (link === activeLink) {
         link.classList.add("active");
@@ -40,33 +43,30 @@ function initializeWebsite() {
     });
   }
 
-  //add click event listener to each navigation link
-  navLinks.forEach((link) => {
+
+//add click event listener to each navigation link 
+navLinks.forEach(link => {
     link.addEventListener("click", (e) => {
-      e.preventDefault;
+        e.preventDefault
 
-      const linkText = link.textContent.toLowerCase();
+        const linkText = link.textContent.toLowerCase();
 
-      //Determine which content to load base don which link was clicked
+        //Determine which content to load base don which link was clicked
 
-      if (linkText === "home") {
-        loadHome();
-      } else if (linkText === "about") {
-        loadAbout();
-      } else if (linkText === "menu") {
-        loadMenu();
-      } else if (linkText === "contact") {
-        loadContact();
-      }
+        if(linkText === 'home') {
+            loadHome();
+        } else if (linkText === 'about') {
+            loadAbout();
+        } else if (linkText === 'menu') {
+            loadMenu();
+        } else if (linkText === 'contact') {
+            loadContact();
+        }
 
-      setActiveTab(link); // call setActiveTab
+        setActiveTab(link); // call setActiveTab
+
+
     });
-  });
+});
 
-  //load home page by default when page first loads
-  loadHome();
-  selectActiveTab(navLinks[0]); //highlight home tab by default as well
 }
-
-
-document.addEventListener('DOMContentLoaded', initializeWebsite); //run init when DOM is fully loaded
